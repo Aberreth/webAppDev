9074b3944f44e9f8948eb226587753c2f75a2487
Alek Berreth <aberreth@csi.edu>
2026-09-16 19:30:43 -0600

    Submission of Week 5 homework

--- W5/LastNameHW5/scripts.js
+++ W5/LastNameHW5/scripts.js
@@ -1,16 +1,22 @@
+// Creates a song name string variable, and a play count integer variable.
+// The variables are then concatated into a console output.
 let currentSong="With Hope Crossed On Our Hearts - Toby Fox"
 let songPlays=45
 console.log(currentSong.concat("\n# of plays", songPlays))
 
+// Creates a string and a boolean constant, then concatenates them into a console output.
 let concatString = "Is my playlist good?"
 const isPlaylistGood=true
 console.log(concatString.concat(" ", isPlaylistGood))
 
+// Creates a list and outputs the variable into the console.
 let favSongs=["livestream - horrormovies", "Snooze Button - Snot", "show me the sky. show me how to live - AZALI"]
 console.log("My favorite songs include " + favSongs[0] + ", " + favSongs[1] + ", and " + favSongs[2])
 
+// Creates a really small number so I have a 5th variable type, and uses it in a console output.
 let reallySmallNumber = 0.9/7
 console.log("This is a " + reallySmallNumber + " for no reason! :)")
 
+// Outputs the results of these math problems into the console.
 console.log(9*10)
 console.log(4/2)
