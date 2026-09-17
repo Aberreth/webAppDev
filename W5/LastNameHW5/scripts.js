0d1bd57b121044f36d590d1aab95b05578e45ae1
Alek Berreth <aberreth@csi.edu>
2026-09-16 19:18:11 -0600

    Submission for Homework 5

---
+++ W5/LastNameHW5/scripts.js
@@ -0,0 +1,16 @@
+let currentSong="With Hope Crossed On Our Hearts - Toby Fox"
+let songPlays=45
+console.log(currentSong.concat("\n# of plays", songPlays))
+
+let concatString = "Is my playlist good?"
+const isPlaylistGood=true
+console.log(concatString.concat(" ", isPlaylistGood))
+
+let favSongs=["livestream - horrormovies", "Snooze Button - Snot", "show me the sky. show me how to live - AZALI"]
+console.log("My favorite songs include " + favSongs[0] + ", " + favSongs[1] + ", and " + favSongs[2])
+
+let reallySmallNumber = 0.9/7
+console.log("This is a " + reallySmallNumber + " for no reason! :)")
+
+console.log(9*10)
+console.log(4/2)
